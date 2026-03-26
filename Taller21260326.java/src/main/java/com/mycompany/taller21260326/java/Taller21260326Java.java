3302a3b1be65d84bca09a7d66ade227b832d44e5
Katherine Ortega <est.katherine.ortega@unimilitar.edu.co>
2026-03-26 09:59:23 -0500

    implemente la clase nodo

---
+++ Taller21260326.java/src/main/java/com/mycompany/taller21260326/java/Taller21260326Java.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller21260326.java;
+
+/**
+ *
+ * @author Katherine Ortega 
+ * 6014550
+ */
+public class Taller21260326Java {
+
+ public class Nodo {
+    int dato;
+    Nodo siguiente;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+}
